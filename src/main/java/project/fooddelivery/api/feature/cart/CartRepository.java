5600f7bc24c6021863dec468ddffa3a7c3ebcfca
mohamed-el3arapy <mohamedel3arapy@gmail.com>
2026-08-26 18:42:38 +0300

    Update CartRepository.java

--- src/main/java/project/fooddelivery/api/feature/cart/CartRepository.java
+++ src/main/java/project/fooddelivery/api/feature/cart/CartRepository.java
@@ -1,9 +1,9 @@
 package project.fooddelivery.api.feature.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 import java.util.UUID;
 
-interface CartRepository extends JpaRepository<Cart, UUID> {
-
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Optional<Cart> findByCustomerId(UUID customerId);
 }
