99b5102e7838a2adf8d3801f182262d45a80d2d1
mohamed-el3arapy <mohamedel3arapy@gmail.com>
2026-08-26 18:31:58 +0300

    Delete CartRepository.java

--- CartRepository.java
+++
@@ -1,9 +0,0 @@
-package project.fooddelivery.api.feature.cart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface CartRepository extends JpaRepository<Cart, UUID> {
-    Optional<Cart> findByCustomerId(String customerId);
-}
