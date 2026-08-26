4e1a23975b3657b0d63e0f3b944580c8432f3bfc
mohamed-el3arapy <mohamedel3arapy@gmail.com>
2026-08-26 18:41:16 +0300

    Update CartController.java

--- src/main/java/project/fooddelivery/api/feature/cart/CartController.java
+++ src/main/java/project/fooddelivery/api/feature/cart/CartController.java
@@ -1,16 +1,22 @@
 package project.fooddelivery.api.feature.cart;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
 @RestController
-@RequestMapping("cart")
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
 public class CartController {
 
     private final CartService cartService;
+    private final CartMapper cartMapper;
 
-
-
+    @PostMapping("/add")
+    public ResponseEntity<CartItemResponse> addToCart(@Valid @RequestBody AddToCartRequest request) {
+        CartItem savedItem = cartService.addToCart(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartMapper.toCartItemResponse(savedItem));
+    }
 }
