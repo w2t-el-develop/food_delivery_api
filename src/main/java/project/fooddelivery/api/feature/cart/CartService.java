dd5af3f2a4848bd3f647463fa9f6153fb9b08d54
mohamed-el3arapy <mohamedel3arapy@gmail.com>
2026-08-26 18:43:12 +0300

    Update CartService.java

--- src/main/java/project/fooddelivery/api/feature/cart/CartService.java
+++ src/main/java/project/fooddelivery/api/feature/cart/CartService.java
@@ -2,17 +2,31 @@ package project.fooddelivery.api.feature.cart;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-class CartService {
+public class CartService {
 
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
 
-//    Cart getCartByCustomerId(String customerId){
-//
-//    }
-//
+    @Transactional
+    public CartItem addToCart(AddToCartRequest request) {
+        Cart cart = cartRepository.findByCustomerId(request.customerId())
+                .orElseGet(() -> {
+                    Cart newCart = new Cart();
+                    newCart.setCustomerId(request.customerId());
+                    return cartRepository.save(newCart);
+                });
 
+        CartItem newItem = CartItem.builder()
+                .cart(cart)
+                .menuItemId(request.cartItem().menuItemId())
+                .quantity(request.cartItem().quantity())
+                .price(request.cartItem().price())
+                .build();
 
+        return cartItemRepository.save(newItem);
+    }
 }
